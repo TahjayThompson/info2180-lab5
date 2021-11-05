f84cbf64c705069c9202b5c2b992fa6355d07aac
tahjaythompson <thompsontahjay34@gmail.com>
2021-11-05 17:23:46 -0500

    created table

--- world.php
+++ world.php
@@ -16,6 +16,29 @@ $countryQ= $country->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
+<table>
+    <tr>
+      <th> Country Name</th>  
+      <th> Continent</th>  
+      <th> Indenpendence Year</th>  
+      <th> Head of State</th>  
+    </tr>
+    
+    <tbody>
+    <?php foreach ($countryQ as $place): ?>
+        <tr>
+            <td> <?= $place['name']; ?></td>  
+            <td> <?= $place['continent']; ?></td>  
+            <td> <?= $place['independence_year']; ?></td>  
+            <td> <?= $place['head_of_state']; ?></td>  
+        </tr>
+     <?php endforeach; ?>
+    </tbody>
+
+</table>
+
+
+
 <ul>
 <?php foreach ($countryQ as $check): ?>
   <li><?= $check['name']; ?></li>
