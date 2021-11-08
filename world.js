ca06fb6d452b0ee37de7841c5f9f82f131c2a734
tahjaythompson <thompsontahjay34@gmail.com>
2021-11-08 09:39:56 -0500

    Final code

--- world.js
+++ world.js
@@ -2,6 +2,7 @@ window.onload = function() {
 
   const  input = document.getElementById('country');
   const  lookUp = document.getElementById('lookup');
+  const  cities = document.getElementById('cities');
   const  result = document.getElementById('result');
   const  httpReq= new XMLHttpRequest();
    
@@ -10,19 +11,27 @@ window.onload = function() {
 	httpReq.open('GET', url);
 	httpReq.send();
   }
-  lookUp.addEventListener('click', function(event) {
-	event.preventDefault();
-	const searchText = input.value;
-	let stored = searchText.trim();
-	const  url = `world.php?country=${stored}`;
-	REG(url);
-	console.log(stored)
 
-
+    lookUp.addEventListener('click', function(event) {
+    event.preventDefault();
+    const searchText = input.value;
+    let stored = searchText.trim();
+    const  url = `world.php?country=${stored}`;
+    REG(url);
   });
 
+    cities.addEventListener('click', function(event) {
+    event.preventDefault();
+    const searchText = input.value;
+    let save = searchText.trim();
+    const  url = `world.php?country=${save}&context=${save}`;
+    REG(url);
+  });
 
-  function callback() {
+  
+  
+  const callback=()=> {
+    input.value = '';
     if (httpReq.readyState === XMLHttpRequest.DONE) {
       if (httpReq.status === 200) {
         const res = httpReq.responseText;
--- world.php
+++ world.php
@@ -8,48 +8,63 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 $stmt = $conn->query("SELECT * FROM countries");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$reqCountry=trim(filter_var(htmlspecialchars($_GET['country']), FILTER_SANITIZE_STRING));
-$reqContext =trim(filter_var(htmlspecialchars($_GET['context']), FILTER_SANITIZE_STRING)); 
 
+
+$reqCountry=trim(filter_var(htmlspecialchars($_GET['country']), FILTER_SANITIZE_STRING));
 $country= $conn->query("SELECT * FROM countries WHERE name LIKE '%$reqCountry%'");
 $countryQ= $country->fetchAll(PDO::FETCH_ASSOC);
+
+$reqContext =trim(filter_var(htmlspecialchars($_GET['context']), FILTER_SANITIZE_STRING)); 
+$cities= $conn->query("SELECT cities.population, cities.district ,cities.name FROM cities JOIN countries ON countries.code=cities.country_code WHERE countries.name LIKE '%$reqContext%'");
+$citiesQ= $cities->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 
-<table>
-    <tr>
-      <th> Country Name</th>  
-      <th> Continent</th>  
-      <th> Indenpendence Year</th>  
-      <th> Head of State</th>  
-    </tr>
-    
-    <tbody>
-    <?php foreach ($countryQ as $place): ?>
+
+  <?php if (isset($_GET['country']) && !isset($_GET['context'])):  ?>
+    <table>
         <tr>
-            <td> <?= $place['name']; ?></td>  
-            <td> <?= $place['continent']; ?></td>  
-            <td> <?= $place['independence_year']; ?></td>  
-            <td> <?= $place['head_of_state']; ?></td>  
+          <th> Country Name</th>  
+          <th> Continent</th>  
+          <th> Indenpendence Year</th>  
+          <th> Head of State</th>  
         </tr>
-     <?php endforeach; ?>
-    </tbody>
+        
+        <tbody>
+        <?php foreach ($countryQ as $place): ?>
+            <tr>
+                <td> <?= $place['name']; ?></td>  
+                <td> <?= $place['continent']; ?></td>  
+                <td> <?= $place['independence_year']; ?></td>  
+                <td> <?= $place['head_of_state']; ?></td>  
+            </tr>
+         <?php endforeach; ?>
+        </tbody>
+    </table>
+        
+    <?php elseif (isset($_GET['context']) && isset($_GET['country'])):?>
+        <table>
+            <tr>
+              <th> Name</th>  
+              <th> District</th>  
+              <th> Popululation</th>  
+            </tr>
+            
+            <tbody>
+            <?php foreach ($citiesQ as $town): ?>
+                <tr>
+                    <td> <?= $town['name']; ?></td>  
+                    <td> <?= $town['district']; ?></td>  
+                    <td> <?= $town['population']; ?></td>  
+                </tr>
+             <?php endforeach; ?>
+            </tbody>
+        </table>
+<?php endif ?>
 
-</table>
 
 
-
-<ul>
-<?php foreach ($countryQ as $check): ?>
-  <li><?= $check['name']; ?></li>
-<?php endforeach; ?>
-</ul>
-
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
 
 
 
